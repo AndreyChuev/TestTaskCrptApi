e2d631827ab34292b3e9a2eed18318cd9ab126a1
Andrew Chuev <chuev.dev@gmail.com>
2024-03-01 22:31:39 +0300

    Решено тестовое задание

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,7 @@ import java.util.concurrent.TimeUnit;
 public class Main {
     public static void main(String[] args) {
         CrptApi crptApi = new CrptApi(TimeUnit.MINUTES, 30);
+        CrptApi.RussianProductDocument emptyProductDocument = new CrptApi.RussianProductDocument();
+        crptApi.createProductEntryDocument(emptyProductDocument);
     }
 }
